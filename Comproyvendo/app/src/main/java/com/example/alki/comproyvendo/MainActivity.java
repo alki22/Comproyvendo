97ca64bd6ac719a6e1b895eed9ed3bc7d59c5608
alki22 <alki.collias@gmail.com>
2016-05-28 20:33:37 -0300

    Replaced activity chosing system and access to Activity1 added

--- Comproyvendo/app/src/main/java/com/example/alki/comproyvendo/MainActivity.java
+++ Comproyvendo/app/src/main/java/com/example/alki/comproyvendo/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,39 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DBHandler db = new DBHandler(this);
-
-        // Inserting Shop/Rows
-        Log.d("Insert: ", "Inserting ..");
-        db.addProduct(new Product("Comics", 200));
-        db.addProduct(new Product("Donuts", 150));
-        db.addProduct(new Product("Pizza", 456));
-        db.addProduct(new Product("Rolls Royce", 240000));
-
-        // Reading all shops
-        Log.d("Reading: ", "Reading all products..");
-        List<Product> products = db.getAllProducts();
-
-        for (Product product : products) {
-            String log = "Id: " + product.getId() + " ,Name: " + product.getName() + " ,Address: "
-                    + product.getPrice();
-            // Writing shops  to log
-            Log.d("Product: : ", log);
-        }
     }
 
-    public void openActivity(View view) {
+    public void openActivityOne(View view) {
         int viewId = view.getId();
-        Intent intent = null;
-        String button = getResources().getResourceEntryName(viewId);
-        switch (button) {
-            case ("button1"):
-                intent = new Intent(this, Activity1.class);
-                startActivity(intent);
-            case ("button2"):
-                Log.d("Id:", "value: " + button);
-            case ("button3"):
-                Log.d("Id:", "value: " + button);
-        }
+        Intent intent = new Intent(this, Activity1.class);
+        startActivity(intent);
+    }
+
+    public void openActivityThree(View view) {
+        int viewId = view.getId();
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
     }
 }
